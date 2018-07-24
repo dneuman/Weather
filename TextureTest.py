0b2ace48943c0c8fca86c574e0e83d3796fe2230
Dan Neuman <659878+dneuman@users.noreply.github.com>
2018-07-24 13:49:14 -0400

    _hash3 ready for testing
    _hash3 uses indices instead of array templates to randomly distribute added points. This reduces the looping required, and should speed up operation.

--- TextureTest.py
+++ TextureTest.py
@@ -33,10 +33,12 @@ class Texture(object):
             prob /= 4.
             self.choice = [1. - dark, 1.]
             self.p = [prob, 1. - prob]
-        elif style == 'hash2':
+        elif style == 'hash2' or style == 'hash3':
             # hash2 spreads out points somewhat evenly, but random within
             # 3x3 blocks. Up to 2 points are chosen per block.
             # Set up arrays and their probabilities
+            self.space = kwargs.get('space', 4)
+            self.frac = kwargs.get('frac', .8)  # fraction of spaces to fill
             self.p = 9 * [1./11] + [2./11]  # 2 chances to get blank
             self.choice = list(range(9)) + [-1]
             self.clip = (1. - dark)**3  # clip limit (max darkness)
@@ -46,6 +48,13 @@ class Texture(object):
             for i in range(9):
                     self.template[i] = blank
                     blank = np.roll(blank, 1)
+            # make list of directions and pairs of directions.
+            # Directions are tuples of (dtn, axis) for np.roll
+            self.dtns = [(-1, 0), (-1, 1), (1, 0), (1, 1)]  # directions
+            self.pairs = []  # list of pairs of directions
+            for i in range(3):
+                for j in range(i+1,4):
+                    self.pairs.append([self.dtns[i], self.dtns[j]])
         else:
             self.choice = list(1. - np.array([1,2,3]) * dark) + [1.]
             self.p = 3 * [prob] + [1. - 3 * prob]
@@ -55,6 +64,7 @@ class Texture(object):
         if self.style == 'noise': return self._noise(im), 0, 0
         if self.style == 'hash': return self._hash(im), 0, 0
         if self.style == 'hash2': return self._hash2(im), 0, 0
+        if self.style == 'hash3': return self._hash3(im), 0, 0
         return im, 0, 0  # Do nothing if style not found
 
     def _expand(self, small, large):
@@ -171,6 +181,63 @@ class Texture(object):
             rgb *= noise  # (nx, ny, 3) = (nx, ny, 3) * (nx, ny, 1)
         return self._combine(rgb, clip)
 
+    def _hash3(self, im):
+        # Speed up _hash2
+        n = self.block
+        rgb = im[...,:3]  # (nx, ny, 3)
+        clip = im[...,3]   # (nx, ny)
+        nx, ny = clip.shape
+        shape = (nx//n, ny//n)
+        # calculate number of points to be plotted.
+        plen = shape[0]//(self.space-1) * shape[1]//(self.space-1)
+        p = np.arange(plen)
+        ix = []
+        iy = []
+        for i in range(2): # Do this for each set of points
+            # make the indices for the point array, spread out by self.space
+            ix.append(p//(self.space-1) * self.space)
+            iy.append(p % (self.space-1) * self.space)
+            # now put them randomly in the spaces
+            ix[i] += rnd.randint(0, self.space, plen)
+            iy[i] += rnd.randint(0, self.space, plen)
+        # a[ix, iy] will be the locations of random but evenly spread points.
+        # Next step is to randomly choose these points to be given one of
+        # the 4 possible directions. p is already an index into the points,
+        # so just scramble it up and divide into 4 groups. Choosing fewer
+        # points will create blank spots.
+        # But I want 2 points per space, and I don't want to repeat the
+        # directions used. This makes 6 possible pairs of directions, so
+        # create 2 sets of points and divide them into 6 groups by reshaping
+        # the vectors into (n//6, 6) arrays.
+        r = []  # This will hold vectors of randomized indices
+        slen = (plen * self.frac)//6  # length of subsets
+        rlen = slen * 6  # total length of randomize indices
+        for i in range(2):
+            temp = rnd.choice(p, plen, replace=False)
+            temp = temp[:rlen]  # truncate to needed size
+            temp.reshape((slen, 6))
+            r.append(temp.copy())
+        # Now stamp the points onto a buffer
+        buffer = np.ones(shape)
+        for i in range(6):
+            for j in range(2):
+                sub = np.ones(shape)
+                dtn, axis = self.pairs[i][j]
+                sub[ix[j][r[:,i]], iy[j][r[:,i]]] = self.dark
+                buffer *= sub
+                s2 = sub * sub
+                buffer *= np.roll(s2, dtn, axis)
+                buffer *= np.roll(s2 * sub, 2 * dtn, axis)
+
+        np.clip(buffer, self.choice[0]**3, 1, buffer)
+        noise = np.ones((nx, ny, 1))
+        self._expand(buffer, noise[..., 0])
+        if self.light:
+            rgb = self._light(rgb, noise[..., 0])
+        else:
+            rgb *= noise  # (nx, ny, 3) = (nx, ny, 3) * (nx, ny, 1)
+        return self._combine(rgb, clip)
+
 def test(style, **kwargs):
     global filt
     filt = Texture(style=style, **kwargs)
@@ -205,14 +272,14 @@ def texture_pie(ax):
 
 
 if __name__=='__main__':
-#    test('hash2')
-    fig = plt.figure(figsize=(8,8))
-    fig.clear()
-    plt.subplots_adjust(left=0.05, right=0.95)
-
-    ax = fig.add_subplot(111)
-    ax.set_aspect(1)
-    texture_pie(ax)
-    ax.set_frame_on(True)
-
-    plt.show()
+    test('hash3')
+#    fig = plt.figure(figsize=(8,8))
+#    fig.clear()
+#    plt.subplots_adjust(left=0.05, right=0.95)
+#
+#    ax = fig.add_subplot(111)
+#    ax.set_aspect(1)
+#    texture_pie(ax)
+#    ax.set_frame_on(True)
+#
+#    plt.show()
